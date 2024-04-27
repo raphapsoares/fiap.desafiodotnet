f79da347f41a7eb5d4d137f63092a3da2da66e85
raphapsoares <rafa_foxx@hotmail.com>
2024-04-27 16:33:42 -0300

    ajuste pontual

--- src/app/fiap.testedotnet.api/Controllers/RelacionamentoController.cs
+++ src/app/fiap.testedotnet.api/Controllers/RelacionamentoController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace fiap.testedotnet.api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class RelacionamentoController : ControllerBase
     {
